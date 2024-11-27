2a3733c80e4c4a9d066bf9594c0b2655951b368c
kminhoo <whdgus4158@gmail.com>
2024-11-27 19:16:39 +0900

    fix: Protected, Public Route 컴포넌트 에러 수정

--- src/components/router/ProtectedRoute.jsx
+++ src/components/router/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 import useAuthStore from '../../store/authStore'
 
 const ProtectedRoute = () => {
-  const { isLoggedIn } = useAuthStore((state) => state.isLoggedIn)
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn)
 
   if (!isLoggedIn) {
     return <Navigate to="/login" replace />
--- src/components/router/PublicRoute.jsx
+++ src/components/router/PublicRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 import useAuthStore from '../../store/authStore'
 
 const PublicRoute = () => {
-  const { isLoggedIn } = useAuthStore((state) => state.isLoggedIn)
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn)
 
   if (isLoggedIn) {
     return <Navigate to="/main" replace />
